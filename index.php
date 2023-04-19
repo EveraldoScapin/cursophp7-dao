3bf4998c5c14574205ae3b49b197d6ae47514dbf
Everaldo Scapin <everaldo.scp@gmail.com>
2023-04-19 13:29:41 -0300

    métodos de lista

--- index.php
+++ index.php
@@ -2,15 +2,27 @@
 
 	require_once("config.php");
 
-	$root = new Usuario();
-
-	$root->loadById(3);
-
-	echo $root;
+	//Carrega um usuário
+	//$root = new Usuario();
+	//$root->loadById(3);
+	//echo $root;
 
 	/*$sql = new Sql();
 	$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 	echo json_encode($usuarios); ESSE BLOCO CONSULTA TODOS OS USARIOS*/ 
 
+	//Carrega uma lista de usuarios 
+
+	//$lista = Usuario::getList(); //chama direto o método pq é estatico
+	//echo json_encode($lista);
+
+	//Carrega uma lista de usuários buscando pelo login
+	//$search = Usuario::search("jo");
+	//echo json_encode($search);
+
+	//Carrega usuario usando login e senha
+	$usuario = new Usuario();
+	$usuario->login("root", "!@$%#");
+	echo $usuario;
 
  ?>
